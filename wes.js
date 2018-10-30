f73b77e6c8b5e53e9aabd28cb5a8b53d75de20ca
titanicwork123 <44313265+titanicwork123@users.noreply.github.com>
2018-10-30 20:09:47 +0200

    Update wes.js

--- wes.js
+++ wes.js
@@ -1,1 +1,53 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
 
+const adminprefix = "#";
+const devs = ['498177367152263178', '498177367152263178'];
+ 
+
+client.on('message', message => {
+  var argresult = message.content.split(` `).slice(1).join(' ');
+    if (!devs.includes(message.author.id)) return;
+    
+if (message.content.startsWith(adminprefix + 'ply')) {
+  client.user.setGame(argresult);
+    message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+} else 
+  if (message.content === (adminprefix + "Percie")) {
+  message.guild.leave();        
+} else  
+if (message.content.startsWith(adminprefix + 'wt')) {
+client.user.setActivity(argresult, {type:'WATCHING'});
+    message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+} else 
+if (message.content.startsWith(adminprefix + 'ls')) {
+client.user.setActivity(argresult , {type:'LISTENING'});
+    message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+} else     
+  if (message.content.startsWith(adminprefix + 'setname')) {
+client.user.setUsername(argresult).then
+    message.channel.sendMessage(`**${argresult}** : Done :>`)
+return message.reply("**You Can't Change Your Name ,Only After Two Hours :>**");
+} else
+  if (message.content.startsWith(adminprefix + 'setavatar')) {
+client.user.setAvatar(argresult);
+  message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
+      } else     
+if (message.content.startsWith(adminprefix + 'st')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/idk");
+    message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+}
+  if(message.content === adminprefix + "restart") {
+    if (!devs.includes(message.author.id)) return;
+        message.channel.send(`:warning:️ **Bot restarting by ${message.author.username}**`);
+      console.log("\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+      console.log(`⚠️ Bot restarting... ⚠️`);
+      console.log("===============================================\n\n");
+      client.destroy();
+      child_process.fork(__dirname + "/bot.js");
+      console.log(`Bot Successfully Restarted`);
+  }
+
+});
+
+client.login('NDk4MTc3MzY3MTUyMjYzMTc4.DrcT6A.pHxgRRZ3GoqO-AiAoqjQaBIH4s8');
